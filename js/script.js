35a1ca99f09e7a4f2f312e3ee79f4229a3f6a905
zhongshan <imzhongshan@126.com>
2016-11-10 17:11:27 +0800

    不画logo了

--- js/script.js
+++ js/script.js
@@ -3,14 +3,13 @@ var context = chess.getContext('2d');
 
 context.strokeStyle = '#bfbfbf';
 
-var logo = new Image();
-logo.src = "images/logo.png";
-logo.onload = function() {
-  context.drawImage(logo, 0, 0, 450, 450);
+// var logo = new Image();
+// logo.src = "images/logo.png";
+// logo.onload = function() {
+  // context.drawImage(logo, 0, 0, 450, 450);
   drawChessBoard();
-}
-
-var drawChessBoard = function() {
+// }
+function drawChessBoard() {
   for (var i=0; i<15; i++) {
     context.moveTo(15+i*30, 15);
     context.lineTo(15+i*30, 435);
