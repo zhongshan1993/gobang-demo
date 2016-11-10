a4b4a5f71f9ce4ff3e8ff06d74e7cdc666911f5f
zhongshan <imzhongshan@126.com>
2016-11-10 21:36:32 +0800

    画棋子

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ var context = chess.getContext('2d');
 
 context.strokeStyle = '#bfbfbf';
 
-// var logo = new Image();
-// logo.src = "images/logo.png";
-// logo.onload = function() {
-  // context.drawImage(logo, 0, 0, 450, 450);
-  drawChessBoard();
-// }
+
+drawChessBoard();
+oneStep(0, 0, true);
+oneStep(1, 1, false);
+
+
 function drawChessBoard() {
   for (var i=0; i<15; i++) {
     context.moveTo(15+i*30, 15);
@@ -18,4 +18,21 @@ function drawChessBoard() {
     context.lineTo(435, 15+i*30);
     context.stroke();
   }
+}
+
+function oneStep(i, j, me) {
+  // 画圆
+  context.beginPath();
+  context.arc(15+i*30, 15+j*30, 13, 0, 2*Math.PI);
+  context.closePath();
+  var gradient = context.createRadialGradient(15+i*30+2, 15+j*30-2, 13, 15+i*30+2, 15+j*30-2, 0);
+  if (me) {
+    gradient.addColorStop(0, '#0A0A0A');
+    gradient.addColorStop(1, '#636766');
+  } else {
+    gradient.addColorStop(0, '#D1D1D1');
+    gradient.addColorStop(1, '#F9F9F9');
+  }
+  context.fillStyle = gradient;
+  context.fill(); // 填充
 }
