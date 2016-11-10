197964e5fc6a70b60361dffc8d363882b69b2a27
zhongshan <imzhongshan@126.com>
2016-11-11 02:00:42 +0800

    ai

--- js/script.js
+++ js/script.js
@@ -1,13 +1,105 @@
 var chess = document.getElementById('chess');
 var context = chess.getContext('2d');
+var me = true;
+var over = false;
+var chessBoard = [];
 
+// 赢法数组
+var wins = [];
+
+// 赢法统计数组
+var myWin = [],
+    computerWin = [];
+
+for (var i=0; i<15; i++) {
+  wins[i] = [];
+  for (var j=0; j<15; j++) {
+    wins[i][j] = [];
+  }
+}
+
+var count = 0;
+// 横向赢法
+for (var i=0; i<15; i++) {
+  for (var j=0; j<11; j++) {
+    for (var k=0; k<5; k++) {
+      wins[i][j+k][count] = true;
+    }
+    count++;
+  }
+}
+// 竖向赢法
+for (var i=0; i<15; i++) {
+  for (var j=0; j<11; j++) {
+    for (var k=0; k<5; k++) {
+      wins[j+k][i][count] = true;
+    }
+    count++;
+  }
+}
+// 斜向赢法
+for (var i=0; i<11; i++) {
+  for (var j=0; j<11; j++) {
+    for (var k=0; k<5; k++) {
+      wins[i+k][j+k][count] = true;
+    }
+    count++;
+  }
+}
+// 反斜向赢法
+for (var i=0; i<11; i++) {
+  for (var j=14; j>3; j--) {
+    for (var k=0; k<5; k++) {
+      wins[i+k][j-k][count] = true;
+    }
+    count++;
+  }
+}
+
+for (var i=0; i<count; i++) {
+  myWin[i] = 0;
+  computerWin[i] = 0;
+}
+console.log(count);
+
+for (var i=0; i<15; i++) {
+  chessBoard[i] = [];
+  for (var j=0; j<15; j++) {
+    chessBoard[i][j] = 0;
+  }
+}
 context.strokeStyle = '#bfbfbf';
-
-
 drawChessBoard();
-oneStep(0, 0, true);
-oneStep(1, 1, false);
+chess.onclick = function(e) {
+  if(over || !me) return;
+  var x = e.offsetX,
+      y = e.offsetY;
+  var i = Math.floor(x/30),
+      j = Math.floor(y/30);
 
+  if (chessBoard[i][j] == 0) {
+    oneStep(i, j, me);
+    if (me) {
+      chessBoard[i][j] = 1; // 黑子
+    } else {
+      chessBoard[i][j] = 2; // 白子
+    }
+    for (var k=0; k<count; k++) {
+      if (wins[i][j][k]) {
+        myWin[k]++;
+        computerWin[k] = 6; // 异常
+        if (myWin[k] == 5) {
+          window.alert('你赢了！');
+          over = true;
+        }
+      }
+    }
+    if (!over) {
+      me = !me;
+      commputerAI();
+    }
+  }
+}
 
 function drawChessBoard() {
   for (var i=0; i<15; i++) {
@@ -35,4 +127,87 @@ function oneStep(i, j, me) {
   }
   context.fillStyle = gradient;
   context.fill(); // 填充
+}
+
+function commputerAI() {
+  var myScore=  [],
+      computerScore = [];
+  var max = 0;
+  var u=0, v=0;
+  for (var i=0; i<15; i++) {
+    myScore[i] = [];
+    computerScore[i] = [];
+    for (var j=0; j<15; j++) {
+      myScore[i][j] = 0;
+      computerScore[i][j] = 0;
+    }
+  }
+
+  for (var i=0; i<15; i++) {
+    for (var j=0; j<15; j++) {
+      if (chessBoard[i][j] == 0) {
+        for (var k=0; k<count; k++) {
+          if (wins[i][j][k]) {
+            if (myWin[k] == 1) {
+              myScore[i][j] += 200;
+            } else if (myWin[k] == 2) {
+              myScore[i][j] += 400;
+            } else if (myWin[k] == 3) {
+              myScore[i][j] += 2000;
+            } else if (myWin[k] == 4) {
+              myScore[i][j] += 10000;
+            }
+
+            if (computerWin[k] == 1) {
+              computerScore[i][j] += 220;
+            } else if (computerWin[k] == 2) {
+              computerScore[i][j] += 420;
+            } else if (computerWin[k] == 3) {
+              computerScore[i][j] += 2100;
+            } else if (computerWin[k] == 4) {
+              computerScore[i][j] += 20000;
+            }
+          }
+        }
+
+        if (myScore[i][j] > max) {
+          max = myScore[i][j];
+          u = i;
+          v = j;
+        } else if (myScore[i][j] = max) {
+          if (computerScore[i][j] > computerScore[u][v]) {
+            u = i;
+            v = j;
+          }
+        }
+
+        if (computerScore[i][j] > max) {
+          max = computerScore[i][j];
+          u = i;
+          v = j;
+        } else if (computerScore[i][j] = max) {
+          if (myScore[i][j] > myScore[u][v]) {
+            u = i;
+            v = j;
+          }
+        }
+      }
+    }
+  }
+  oneStep(u, v, false);
+  chessBoard[u][v] = 2;
+
+  for (var k=0; k<count; k++) {
+    if (wins[u][v][k]) {
+      computerWin[k]++;
+      myWin[k] = 6; // 异常
+      if (computerWin[k] == 5) {
+        window.alert('计算机赢了！');
+        over = true;
+      }
+    }
+  }
+  if (!over) {
+    me = !me;
+  }
 }
